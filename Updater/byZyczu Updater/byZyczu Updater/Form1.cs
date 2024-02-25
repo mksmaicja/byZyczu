8b3ca75a7f4011cdae2caf287164f2e6303c9186
maicja <maicja@mksteam.ovh>
2024-02-25 14:16:08 +0100

    changing repo struct

---
+++ Updater/byZyczu Updater/byZyczu Updater/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace byZyczu_Updater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebClient client = new WebClient();
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Aktualizowanie Launchera...";
+            client.DownloadFile("")
+        }
+    }
+}
